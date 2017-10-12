32c9d2ffe8cb26cb877f589b8a182df076a59775
khanh <khanhh.nguyenhuu@gmail.com>
2017-10-12 16:35:36 +0700

    toan_tu_trong_php

---
+++ toan_tu_trong_php.php
@@ -0,0 +1,26 @@
+<?php
+  $a = 10;
+  $b = 20;
+  $tong = $a + $b;
+  $hieu = $a - $b;
+  $tich = $a * $b;
+  $thuong = $a / $b;
+  $chiadu = $b % $a;
+  $tang = ++$a;
+  $giam = --$a;
+  echo "Tong la : $a + $b = $tong<br>";
+  echo "*************************<br>";
+  echo "Tong la : $a - $b = $hieu<br>";
+  echo "*************************<br>";
+  echo "Tong la : $a * $b = $tich<br>";
+  echo "*************************<br>";
+  echo "Tong la : $a / $b = $thuong<br>";
+  echo "*************************<br>";
+  echo "Tong la : $b % $a = $chiadu<br>";
+  echo "*************************<br>";
+  echo "Tang $a 1 don vi : $tang<br>";
+  echo "*************************<br>";
+  echo "Giam $tang 1 don vi : $giam<br>";
+  echo "*************************<br>";
+
+?>
