9a2311bb2db970f6a6aa269c191f5522b3dee8dc
khanh <khanhh.nguyenhuu@gmail.com>
2017-10-12 14:51:37 +0700

    them cu-phap.php

---
+++ cu-phap.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Ví dụ chương trình PHP</title>
+    </head>
+    <body>
+      <?php
+        echo "ví dụ minh họa lệnh echo! <br>";
+  	    print "ví dụ minh họa lệnh print!";
+  	  ?>
+    </body>
+  </html>
