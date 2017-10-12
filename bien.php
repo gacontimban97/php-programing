3fdedbcca8bafeeeb272b0ba8bccfb6adfafcc97
khanh <khanhh.nguyenhuu@gmail.com>
2017-10-12 15:59:23 +0700

    Bien trong php

---
+++ bien.php
@@ -0,0 +1,18 @@
+<?php
+  define("PI", 3.14);
+  define("HOTGIRL", 'gai dep');
+
+  $name = "Nguyễn Hữu Khánh";
+  $address = "Huế";
+  $old = "20";
+
+  echo "Tên : $name <br>";
+  echo "****************<br>";
+  echo "Địa Chỉ : $address <br>";
+  echo "****************<br>";
+  echo "Tuổi : $old <br>";
+  echo "****************<br>";
+  echo PI;
+  echo "<br>****************<br>";
+  echo HOTGIRL;
+?>
